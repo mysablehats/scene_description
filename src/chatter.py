caa7f7a2ced597c9ce758fa3236c32e58b1ef25d
Frederico B. Klein <frederico.klein@plymouth.ac.uk>
2018-11-27 14:25:32 +0000

    added simple chatter

--- src/chatter.py
+++ src/chatter.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python
 
-# recieves name, value and rate. publishes those. 
+# recieves name, value and rate. publishes those.
+
+# adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    topic_name = rospy.get_param('~topic_name')
+    value = rospy.get_param('~value')
+    rate_value = rospy.get_param('~rate')
+    pub = rospy.Publisher(topic_name, String, queue_size=1)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(rate_value)
+    while not rospy.is_shutdown():
+        #hello_str = "hello world %s" % rospy.get_time()
+        #rospy.loginfo(hello_str)
+        pub.publish(value)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
